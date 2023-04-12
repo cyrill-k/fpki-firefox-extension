4e926e4baff788f4cb8ed8fe64283229ff645992
Cyrill Krähenbühl <cyrill.kraehenbuehl@inf.ethz.ch>
2023-04-12 10:07:27 +0200

    fix access rights

--- db/grant_user_privileges.sql
+++ db/grant_user_privileges.sql
@@ -1,6 +1,6 @@
 USE mysql;
 CREATE USER IF NOT EXISTS 'test'@'%' IDENTIFIED BY 'zaphod';
-GRANT ALL PRIVILEGES ON fpki.* TO 'test'@'%';
+GRANT ALL PRIVILEGES ON *.* TO 'test'@'%';
 -- needed for mapserver
 GRANT SUPER ON *.* TO 'test'@'%';
 -- UPDATE user SET plugin='auth_socket' WHERE User='test';
